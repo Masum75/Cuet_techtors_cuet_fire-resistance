f1f2931507a07a7c13996280163b1e1e28b39b09
Masum75 <99918348+Masum75@users.noreply.github.com>
2022-02-20 09:06:56 +0600

    Update fire_resistance_project.ino

--- fire_resistance_project.ino
+++ fire_resistance_project.ino
@@ -1,6 +1,8 @@
 #include<LiquidCrystal.h>
+#include<Servo.h>
+Servo servo;
 int contrast = 100;
-LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
+LiquidCrystal lcd(0, 1, 2, 4, 5, 6);
 #include <SoftwareSerial.h>
 SoftwareSerial SIM900A(10, 11);
 
@@ -39,27 +41,38 @@ void setup()
   pinMode(buzzer, OUTPUT);
   pinMode(relay, OUTPUT);
    SIM900A.begin(9600);
+   servo.attach(9);
 }
 void loop()
 {
   reading = analogRead(A5);
-   if(reading>150)
+  servo.write(0);
+   if(reading>200)
    {
     digitalWrite(buzzer, HIGH);
+    lcd.clear();
      lcd.setCursor(4, 0);
    lcd.print("Gas leak ");
    lcd.setCursor(4, 1);
    lcd.print("Detected");
+   
    delay(10000);
+   
    digitalWrite(buzzer, LOW);
-   lcd.clear();
+   
+   servo.write(180);
    digitalWrite(relay, HIGH);
    delay(10000);
    digitalWrite(relay, LOW);
+   
    sendSMS();
    call();
-   
    }
-  
-  
+   else{
+    lcd.clear();
+    lcd.setCursor(3, 1);
+    lcd.print("No gas leak");
+    delay(10000);
+   }
+   
 }
